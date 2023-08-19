73eb48f78ba7a75f323f06084d7bc600168e55fc
unknown <pamelataiane1234@gmail.com>
2023-08-18 23:19:38 -0300

    [Variaveis] Atividade sobre variáveis aritmeticos

---
+++ aulas/variaveis/explicacao/variaveis-aritmeticos.js
@@ -0,0 +1,17 @@
+let valorX = 8, valorY = 2;
+
+let resultado = valorX + valorY;
+console.log(resultado); //10
+
+resultado = valorX * valorY;
+console.log(resultado); //16
+
+resultado = valorX / valorY;
+console.log(resultado); //4
+
+resultado = valorX % valorY;
+console.log(resultado); //0
+
+resultado = valorX - valorY;
+console.log(resultado); //6
+
---
+++ aulas/variaveis/explicacao/variaveis-aritmeticos2.js
@@ -0,0 +1,17 @@
+let x = 12;
+
+//x = x + 3; // 15
+x += 3;
+console.log(x); //15
+
+//x = x - 3; //12
+x -= 3;
+console.log(x); //12
+
+// x = x * 3 //36
+x *= 3;
+console.log(x); //36
+
+// x = x / 3 //12
+x /= 3;
+console.log(x); //12

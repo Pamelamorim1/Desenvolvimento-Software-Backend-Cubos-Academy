04032a8ee876409a36fcb9ed19be1b1fb2721d40
unknown <pamelataiane1234@gmail.com>
2023-08-18 23:16:29 -0300

    [Exercicios 01 e 02] Atividades desenvolvidas sobre variáveis

---
+++ aulas/variaveis/exercicios/exercicio02.js
@@ -0,0 +1,5 @@
+let populacaoSalvador = 2_418_005;
+console.log(`Atualmente a população em Salvador(BA) está em ${populacaoSalvador}`);
+
+populacaoSalvador = 2_428_005;
+console.log(`Atualmente a população em Salvador(BA) está em ${populacaoSalvador}`);
